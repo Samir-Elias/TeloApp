20ff44f412e9ce00f8e5996809cf92603c301f8a
Samir Elias <Samireliassalatino@gmail.com>
2025-07-19 16:02:11 -0300

    cambios gemni

--- teloapp-backend/TeloappBackendApplication.java
+++ teloapp-backend/TeloappBackendApplication.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-public class TeloappBackendApplication implements CommandLineRunner { // 1. Implementamos CommandLineRunner
+@Profile("dev") // 0. ¡Añadimos esta línea!
+public class TeloappBackendApplication implements CommandLineRunner {
 
     @Autowired // 2. Inyectamos el repositorio para poder usarlo
     private MotelRepository motelRepository;
